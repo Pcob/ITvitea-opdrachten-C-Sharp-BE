9fe8c34a6a210de0bcdad9a37efe7136a628d2fb
Pcob <71030608+Pcob@users.noreply.github.com>
2020-09-14 21:02:06 +0200

    Add files via upload

---
+++ Oefening 2-4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Csharp
+{
+  
+    public class ExerciseFour
+    {
+        static public void Main()
+        {
+            int EUR, btw, intSom;
+
+            Console.WriteLine("Prijs met BTW berekenen: ");
+            Console.Write("Prijs exclusief BTW: ");
+            EUR = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Type het btw tarief van 21%. Het getal moet 1.21 zijn: ");
+            btw = Convert.ToInt32(Console.ReadLine());
+
+            intSom = EUR * btw;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+
+            Console.WriteLine("Prijs inclusief btw is: " + intSom.ToString());
+            Console.ReadLine();
+
+        }
+    }
+}
