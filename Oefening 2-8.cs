4121b0bce099a0625392ea14943f4fe7350f943b
Pcob <71030608+Pcob@users.noreply.github.com>
2020-09-15 14:53:21 +0200

    Add files via upload

---
+++ Oefening 2-8.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Csharp
+{
+  
+    public class ExerciseEight
+    {
+        static public void Main()
+        {
+            int a, intSom;
+            Double b;
+
+            Console.WriteLine("Prijzen berekenen van auto's inclusief btw naar exclusief btw: ");
+            Console.Write("Typ prijs mercedes inclusief BTW: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = 1.21;
+            
+
+            intSom = a / (int)b;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Prijs mercedes exclusief btw is: " + intSom.ToString());
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
