1209eb4af604c67e6b10931eab9304bb7b6a3a41
Pcob <71030608+Pcob@users.noreply.github.com>
2020-09-15 14:15:17 +0200

    Add files via upload

---
+++ Oefening 2-7.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Csharp
+{
+  
+    public class ExerciseSeven
+    {
+        static public void Main()
+        {
+            int a, b, c, intSom;
+
+            Console.WriteLine("Berekeningen maken: ");
+            Console.Write("Type een getal in: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Type een 2e getal in: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Type een 3e getal in: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+           
+
+            intSom = a * (b/c);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Het totaal is: " + intSom.ToString());
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
