6b5c2448229c3f980e22feea942a1af33b77968a
Pcob <71030608+Pcob@users.noreply.github.com>
2020-09-15 18:52:07 +0200

    Add files via upload

---
+++ Oefening 2-11.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Csharp
+{
+  
+    public class ExerciseTwelve
+    {
+        static void Main(string[] args)
+        {
+            int Omzet, intSom;
+            double variable, vastmaandwedde;
+
+            Console.WriteLine("Bereken maandwedde: ");
+            Console.Write("Geef het omzetbedrag van deze maand weer: ");
+            Omzet = Convert.ToInt32(Console.ReadLine());
+
+            variable = 1.825;
+            vastmaandwedde = 958.36;
+
+
+
+
+            Console.WriteLine("Het maandwedde bedrag is: " + vastmaandwedde + (Omzet * variable));
+            Console.ReadKey();
+        }
+    }
+}
