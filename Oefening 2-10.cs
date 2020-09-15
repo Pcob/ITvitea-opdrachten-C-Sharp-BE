6749b2e8d3096df6c4149152aba7dd6a62579312
Pcob <71030608+Pcob@users.noreply.github.com>
2020-09-15 17:13:44 +0200

    Add files via upload

---
+++ Oefening 2-10.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Csharp
+{
+  
+    public class ExerciseTen
+    {
+        static void Main(string[] args)
+        {
+            int Inkoopprijs, intSom;
+            double winstmarge;
+
+            Console.WriteLine("Berekening van winstmarge op inkoopprijzen");
+            Console.Write("Type inkoopprijs: ");
+            Inkoopprijs = Convert.ToInt32(Console.ReadLine());
+            winstmarge = 1.90;
+           
+
+            intSom = (Inkoopprijs * (int)winstmarge);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Winstmarge is: " + intSom.ToString());
+            Console.ReadKey();
+        }
+    }
+}
